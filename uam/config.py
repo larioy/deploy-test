259a182a496aad518e79dc4c44ae0628260f5d52
larioy <1757207196@qq.com>
2019-05-30 18:52:57 +0800

    chang log

--- uam/config.py
+++ uam/config.py
@@ -8,4 +8,4 @@ env = os.environ.get('BK_ENV', 'development')
 if env.endswith('production'):
     UAM_SITE_URL = '/o/%s/' % UAM_APP_ID
 else:
-    UAM_SITE_URL = '/o/%s/' % UAM_APP_ID
+    UAM_SITE_URL = '/t/%s/' % UAM_APP_ID
